6b09d143fcaf4a8e8cb9ed091cc676b28a868327
OJO-Ahmed <adewunmigenius@gmail.com>
2017-08-26 11:23:03 +0100

    Javascript file

---
+++ app.js
@@ -0,0 +1,18 @@
+// console.log(alert("how far"));
+var button = document.getElementById("button");
+
+button.addEventListener("click", function(){
+	// console.log(alert("ok begin to do"));
+	var inputval = document.getElementById('input').value;
+	//document.getElementById('output').innerHTML = inputval;
+	var selectval = document.getElementById('select').value
+	//create a new div
+	var newdiv = document.createElement("div");
+	//create an input text from the value
+	var text = document.createTextNode(selectval+": "+inputval);
+	//apend text to the div
+	newdiv.appendChild(text);
+	//apend div to #output
+	document.getElementById("output").appendChild(newdiv);
+});
+
